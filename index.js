dc835ef8788be16a062c1b80ac2169c1bda2656a
Laura Trapaga <trapaga9@gmail.com>
2022-12-23 18:15:32 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+// Imports node.modules
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+// Imports objects
+const Manager = require('./lib/manager.js');
+const Engineer = require('./lib/engineer.js');
+const Intern = require('./lib/intern.js');
+
