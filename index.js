8c2058e4aad3892e260b9bb0b3e2fb7c30de23dd
Laura Trapaga <trapaga9@gmail.com>
2022-12-23 19:48:10 -0500

    Add objects and prompt functions to JS code.

--- index.js
+++ index.js
@@ -1,9 +1,140 @@
-// Imports node.modules
-const fs = require('fs');
-const inquirer = require('inquirer');
+// Imports node_modules
+const fs = require("fs");
+const inquirer = require("inquirer");
 
-// Imports objects
-const Manager = require('./lib/manager.js');
-const Engineer = require('./lib/engineer.js');
-const Intern = require('./lib/intern.js');
+// Imports Manager, Engineer, and Intern objects
+const Manager = require("./lib/manager.js");
+const Engineer = require("./lib/engineer.js");
+const Intern = require("./lib/intern.js");
+// Creates an empty array for the data input by the user to push to
+const team = [];
 
+// managerPrompt function
+const managerPrompt = () => {
+  // Imports inquirer
+  return (
+    inquirer
+      .prompt([
+        // Prompts the user to input the manager's name
+        {
+          type: "input",
+          name: "name",
+          message: "What is the manager's name?",
+        },
+        // Prompts the user to input the manager's id
+        {
+          type: "input",
+          name: "id",
+          message: "What is the manager's ID?",
+        },
+        // Prompts the user to input the manager's email
+        {
+          type: "input",
+          name: "email",
+          message: "What is the manager's email?",
+        },
+        // Prompts the user to input the manager's office number
+        {
+          type: "input",
+          name: "officeNumber",
+          message: "What is the manager's office number?",
+        },
+      ])
+      // Gets responses input by the user
+      .then((managerData) => {
+        const { name, id, email, officeNumber } = managerData;
+        // Creates a new object with the values input by the user = manager
+        const manager = new Manager(name, id, email, officeNumber);
+        // Pushes the new manager object to the team array
+        team.push(manager);
+        // Prints the new team array containing the updated manager data to the console
+        console.log(team);
+      })
+  );
+};
+
+// employeePrompt function
+const employeePrompt = () => {
+  // Imports inquirer
+  return (
+    inquirer
+      .prompt([
+        // Prompts the user to choose the employee role type
+        {
+          type: "list",
+          name: "roleType",
+          message: "Which employee are you adding?",
+          choices: ["Engineer", "Intern"],
+        },
+        // Prompts the user to input the employee's name
+        {
+          type: "input",
+          name: "name",
+          message: `What is the employee's name?`,
+        },
+        // Prompts the user to input the employee's id
+        {
+          type: "input",
+          name: "id",
+          message: `What is the employee's id?`,
+        },
+        // Prompts the user to input the employee's email
+        {
+          type: "input",
+          name: "email",
+          message: `What is the employee's email?`,
+        },
+        // If the employee role type is = Engineer, then this will prompt the user to input the employee's github
+        {
+          type: "input",
+          name: "github",
+          message: `What is the employee's github?`,
+          when: (input) => input.roleType === "Engineer",
+        },
+        // If the employee role type is = Intern, then this will prompt the user to input the employee's school
+        {
+          type: "input",
+          name: "school",
+          message: `What is the employee's school?`,
+          when: (input) => input.roleType === "Intern",
+        },
+        // Prompts the user to choose if would like to add another employee's information
+        {
+          type: "confirm",
+          name: "addAnother",
+          message: `Would you like to add another employee?`,
+          default: false,
+        },
+      ])
+      // Gets responses input by the user
+      .then((employeeData) => {
+        let { roleType, name, id, email, github, school, addAnother } =
+          employeeData;
+        // Undefined variable = employee
+        let employee;
+        // If the employee role type = Engineer, then this will create a new object with the values input by the user = employee
+        if (roleType === "Engineer") {
+          employee = new Engineer(name, id, email, github);
+        }
+        // If the employee role type = Intern, then this will create a new object with the values input by the user = employee
+        else if (roleType === "Intern") {
+          employee = new Intern(name, id, email, school);
+        }
+        // Pushes the new employee object to the team array
+        team.push(employee);
+        // If the user chose to add another employee's information, then this will call the employeePrompt function again
+        if (addAnother) {
+          return employeePrompt(team);
+        }
+        // If the user chose not to add another employee's information, then this will print the new team array containing the updated employee data to the console
+        else {
+          return team;
+        }
+      })
+  );
+};
+
+// Calls managerPrompt function
+managerPrompt();
+// Calls employeePrompt function
+employeePrompt();
